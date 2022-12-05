73dea0d60cf1636ec69953024dc34a5c96c2c9f0
Pankaj Goyal <pankajgoyal.ibirdscollege@gmail.com>
2022-12-05 21:49:07 +0530

    add

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
+    user: "root",
+    password: "",
     database: "edu"
 });
 
 con.connect(function (err) {
     if (err) throw err;
+    con.query("Select * From holidays", function (err, result, fields) {
+        if (err) throw err;
+        console.log('err :', err);        
+        console.log('result :', result[0].id);
+        //  console.log('fields :', fields);
+    })
     console.log("Connected!");
 });
